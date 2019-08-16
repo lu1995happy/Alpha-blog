70f556f0c25cab9def0f8514d7dc695999bb93e2
Tianlin Lu <txl355@case.edu>
2019-08-16 10:52:41 -0700

    add admin user requirement test

--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -4,7 +4,8 @@ class CategoriesControllerTest < ActionDispatch::IntegrationTest
   # (if Rails 4 -> class CategoriesControllerTest < ActionController::TestCase)
 
   def setup
-    @category = Category.create(name: "sports")
+    @category = Category.create(name: "Sports")
+    @user = User.create(username: "john", email: "john@example.com", password: "password", admin: true)
   end
 
   test "should get categories index" do
@@ -14,7 +15,9 @@ class CategoriesControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should get new" do
+    sign_in_as(@user, "password")
     get new_category_path
+    # (if Rails 4 -> session[:user_id] = @user.id)
     # (if Rails 4 -> get :new)
     assert_response :success
   end
@@ -23,6 +26,14 @@ class CategoriesControllerTest < ActionDispatch::IntegrationTest
     get category_path(@category)
     # (if Rails 4 -> get(:show, {'id' => @category.id}))
     assert_response :success
+  end
+
+  test "should redirect create when admin not logged in" do
+    assert_no_difference 'Category.count' do
+      post categories_path, params: { category: { name: "sports" } }
+      # (if Rails 4 -> post :create, category: { name: "sports" })
+    end
+    assert_redirected_to categories_path
   end
 
 end
